fc7e2a3eac09f3b3278a8c8fbb3e17d50053fd02
ShivaKrishna <shivakrishna44@gmail.com>
2025-01-23 19:45:09 +0530

    database

--- mysql.sh
+++ mysql.sh
@@ -44,6 +44,11 @@ VALIDATE $? "Starting MySQL Server"
 
 mysql -h mysql.shivakrishna.shop -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 
-echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting Root Password"
+if [ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "MySQL Root password already setup ... $Y SKIPPING $N"
+fi
